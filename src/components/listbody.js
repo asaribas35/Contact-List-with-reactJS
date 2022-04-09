f35b6b8a7ace9c366a0592ff1cb6af80b3125677
alisaribas <alisaribash35@gmail.com>
2022-04-09 13:32:20 +0300

    edit style

--- src/components/listbody.js
+++ src/components/listbody.js
@@ -18,24 +18,26 @@ return(
     <>
     <div className="listBody">
     <div className="row">
-        <div className="col-lg-5 text-center">Name</div>
-        <div className="col-lg-5 text-center">Phone Number</div>
+        <div className="col-5 text-center">Name</div>
+        <div className="col-5 text-center">Phone Number</div>
     </div>
 
   <div>
   {
   filterList
     .map((user, i) => (
+      <div className="card">
+        
+      
+    <div key={i} className="row card-body ">
 
-    <div key={i} className="row">
-
-        <div className="col-sm-5 px-4">{user.name}   </div>
-        <div className="col-sm-5 px-4">{user.number}  </div>
-        <div className="col-sm-2"><button type="button" onClick={deleteItem} className="btn btn-primary btn-sm" value={user.name} >Delete</button></div>
+        <div className="col-5 px-4 text-center">{user.name}   </div>
+        <div className="col-5 px-4 text-center">{user.number}  </div>
+        <div className="col-2 text-center"><button type="button" onClick={deleteItem} className="btn btn-primary btn-sm" value={user.name} >Delete</button></div>
          
         
     </div>
-      
+    </div>
     ))
 }
   </div>
